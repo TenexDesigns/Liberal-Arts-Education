7006a5651b95ec1a3e14a3ce004d12a0cd200f61
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-21 11:09:14 +0300

    Create Article_1_Human_cilivisations.rb

---
+++ Article_1_Human_cilivisations.rb
@@ -0,0 +1,19 @@
+The article emphasizes the value of a Classical Liberal Arts education, highlighting its significance in preparing individuals for an uncertain future dominated by robots and artificial intelligence. The writer argues that while technical skills may change, studying the Liberal Arts can help develop uniquely human qualities such as creativity, empathy, social intelligence, curiosity, storytelling, and adaptability, which cannot be replicated by machines.
+
+The Liberal Arts is a diverse curriculum covering subjects like literature, philosophy, history, and the arts. It is seen as a curriculum that builds strong character, fosters critical thinking, and promotes flexibility in the face of disruptive change.
+
+The article addresses the negative perception that the Liberal Arts often have, being seen as less competitive compared to Science, Technology, Engineering, and Mathematics (STEM) fields. However, it emphasizes that a dedicated study of the Classical Liberal Arts can be challenging and beneficial.
+
+Furthermore, the article suggests that thanks to the internet, it is now possible to gain a Liberal Arts education without the burden of significant student debt. Access to resources like books and knowledge from various cultures is more accessible, enabling self-taught learners to create their own Liberal Arts education.
+
+The article also discusses the meaning of liberalism in the context of education and the freedom and equality of all people.
+
+Additionally, the article presents a 4-year reading list of classical texts from Western perspectives. These texts span different periods in history, including Greek and Roman civilizations, the medieval and Renaissance periods, the Enlightenment, and the modern world. The reading list covers a wide range of subjects, from literature to science, and philosophy to history.
+
+Overall, the article encourages individuals interested in a liberal arts education to explore classical texts and engage in critical thinking and rational debate with others to challenge their own cognitive biases and reality tunnels. It suggests using resources available online to access these texts, which can transform ones worldview and understanding of the modern world.
+
+
+
+--------------------------------------------------------------------
+
+  
